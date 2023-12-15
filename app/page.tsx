1e816b1cc7b0e6ba99e35e3529e94bc283bf4c33
Jay-Karia <jay.sanjay.karia@gmail.com>
2023-12-15 22:02:06 +0530

    Setting up shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <>
       <div>
-        Hello world this is <span className={"text-blue-400"}>Tailwind CSS</span>
+        Hello world this is <span className={"text-blue-400"}>Tailwind CSS</span> and <Button>Shadcn UI</Button>
       </div>
     </>
   )
