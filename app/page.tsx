d2140455ff2053be57d66a861a175fdd75cf2ba8
Jay-Karia <jay.sanjay.karia@gmail.com>
2023-12-15 21:54:46 +0530

    Setting up project

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        Hello world this is <span className={"text-blue-400"}>Tailwind CSS</span>
+      </div>
+    </>
+  )
+}
